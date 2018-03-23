09dd9f256b0910e908bbd713f3b7059b8621da3d
yusifeng <1115372296@qq.com>
2018-03-23 10:09:22 +0800

    '我的脚手架'

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import 'babel-polyfill'
+import fastclick from 'fastclick'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'common/stylus/index.styl'
+import VueLazyLoad from 'vue-lazyload'
+
+Vue.config.productionTip = false
+fastclick.attach(document.body)
+Vue.use(VueLazyLoad, {
+  // loading: require('common/image/default.png')
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
